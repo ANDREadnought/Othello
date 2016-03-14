91e4bc4c80cfe8b934ebb1f446b43628cdb537eb
lukejuusola <luke.juusola@gmail.com>
2016-03-14 03:17:42 -0700

    clear.sh to be used after already extracted

--- clear.sh
+++ clear.sh
@@ -1,9 +1,7 @@
 #! /bin/bash
-./extract.sh
 rm tables/transtable.txt
 rm tables/openingbook.txt
 rm tables/closingbook.txt
 touch tables/transtable.txt
 touch tables/openingbook.txt
 touch tables/closingbook.txt
-./compress.sh
